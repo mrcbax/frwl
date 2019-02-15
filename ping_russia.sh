b4d6cc5b4ca2faafc78a727e2e67e0f94caaafcb
gidoBOSSftw5731 <gidoBOSSftw5731@users.noreply.github.com>
2019-02-14 20:02:03 -0500

    removing --remove-files from tar for freeBSD folks (#7)
    * freeBSD likes the I before the IP
    
    * Missed one
    
    (oops)
    
    * removing --remove-files from tar for freeBSD folks
    
    prevents the following error:
    ```
    traceroute to 178.238.121.241 (178.238.121.241), 64 hops max, 48 byte packets
    tar: --remove-files: Cannot stat: No such file or directory
    tar: Error exit delayed from previous errors.
    ```
    
    * added an RM
    
    used an RM to remove the files but it should only run if the tar returns 0, or so I've been told

--- ping_russia.sh
+++ ping_russia.sh
@@ -53,7 +53,7 @@ _checkPath() {
 
 _tarBall() {
 	#~creates tarball of collected data with id/timestamp range
-	tar cjf "${TARBALL_DIR}/${_randomDir}/${COMP_ITER}.${TIME}.${SERVER}.tar.xz" "${WORKING_DIR}"/* --remove-files
+	tar cjf "${TARBALL_DIR}/${_randomDir}/${COMP_ITER}.${TIME}.${SERVER}.tar.xz" "${WORKING_DIR}"/* && rm "$WORKING_DIR"/*
 	_log date "[_tarBall]created tarball '${COMP_ITER}.${TIME}.${SERVER}.tar.xz'"
 	COMP_ITER=$(( COMP_ITER + 1 ))
 	ITER=0
